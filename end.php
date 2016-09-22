e44305d2edf49d2bafc632d67ed072e32cc06ed3
Maksym Perepelytsya <mperepelytsya@magecore.com>
2016-09-22 16:32:06 +0300

    - add exit() support

--- end.php
+++ end.php
@@ -4,6 +4,7 @@ if (isset($_SERVER['start_cli_script'])) {
 
     $end  = microtime(true);
     $var  = $end - $_SERVER['start_cli_script'];
+    unset($_SERVER['start_cli_script']);
     $time = round
     (
         $var,
--- start.php
+++ start.php
@@ -1,2 +1,18 @@
 <?php
 $_SERVER['start_cli_script'] = microtime(true);
+register_shutdown_function(function () {
+    if (isset($_SERVER['start_cli_script'])) {
+
+        $end  = microtime(true);
+        $var  = $end - $_SERVER['start_cli_script'];
+        unset($_SERVER['start_cli_script']);
+        $time = round
+        (
+            $var,
+            4
+        );
+
+        $format = '%2$stime: %1$.4f%2$s';
+        echo sprintf($format, $time, PHP_EOL);
+    }
+});
